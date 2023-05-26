c567b21e657450388b6dbc75d134e3d78e59b75d
Jakes122 <jakeowiddo@gmail.com>
2023-05-26 09:24:31 -0700

    main.c commit

--- main.c
+++ main.c
@@ -6,9 +6,9 @@
  * @ac: arg count
  * Return: 0 on success, 1 on error
  */
-int main(int ac, int fd, char **av,)
+int main(int ac, , char **av)
 {
-	info_t info[] = { INFO_INIT };
+	info_t info[0] = { INFO_INIT };
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
